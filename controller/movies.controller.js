6af681b421f200ca033ff12bd34ada57830342ab
rymbaniali <rym.ayasrah@hotmail.com>
2021-06-20 13:10:29 +0300

    finish lab 10

--- controller/movies.controller.js
+++ controller/movies.controller.js
@@ -2,25 +2,36 @@ const Movie = require('../modules/movies.module');
 const axios = require('axios');
 require('dotenv').config();
 
-const MOVIES_BIT_KEY= process.env.MOVIES_BIT_KEY;
+const MOVIES_BIT_KEY = process.env.MOVIES_BIT_KEY;
 
-const moviesController= (req, res) => {
+const Cache = require('../helper/cache');
+const cacheOBj = new Cache();
 
-    const city = req.query.city;
-  
-    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIES_BIT_KEY}&query=${city}`;
-  
-  
-    axios.get(movieUrl).then(results => {
-  
-      const responseData = results.data.results.map(obj => new Movie(obj))
-  
-  
-      res.json(responseData);
-  
-    }).catch(error => {
-      res.send(error.message)
-    });
-  };
-  
-module.exports= moviesController;
+const moviesController = (req, res) => {
+
+  const city = req.query.city;
+  if (city) {
+    if (cacheObj[requestKey] && (Date.now() - cacheObj[requestKey].timestamp < 86400000)) {
+      res.json(cacheObj[requestKey].data);
+    } else {
+      const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIES_BIT_KEY}&query=${city}`;
+
+      axios.get(movieUrl).then(results => {
+
+        const responseData = results.data.results.map(obj => new Movie(obj))
+
+
+        res.json(responseData);
+
+      }).catch(error => {
+        res.send(error.message)
+      });
+    };
+
+  }
+
+
+
+
+}
+module.exports = moviesController;
